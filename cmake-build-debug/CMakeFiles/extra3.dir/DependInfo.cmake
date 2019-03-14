85df3e833509a19b1eec14a8d5a4bbf595c70365
Kei98 <keichavez98@gmail.com>
2019-03-13 20:49:13 -0600

    debug

---
+++ cmake-build-debug/CMakeFiles/extra3.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kei98/CLionProjects/Datos/Datos II/extra3/CList.cpp" "/home/kei98/CLionProjects/Datos/Datos II/extra3/cmake-build-debug/CMakeFiles/extra3.dir/CList.cpp.o"
+  "/home/kei98/CLionProjects/Datos/Datos II/extra3/CNode.cpp" "/home/kei98/CLionProjects/Datos/Datos II/extra3/cmake-build-debug/CMakeFiles/extra3.dir/CNode.cpp.o"
+  "/home/kei98/CLionProjects/Datos/Datos II/extra3/collector.cpp" "/home/kei98/CLionProjects/Datos/Datos II/extra3/cmake-build-debug/CMakeFiles/extra3.dir/collector.cpp.o"
+  "/home/kei98/CLionProjects/Datos/Datos II/extra3/list.cpp" "/home/kei98/CLionProjects/Datos/Datos II/extra3/cmake-build-debug/CMakeFiles/extra3.dir/list.cpp.o"
+  "/home/kei98/CLionProjects/Datos/Datos II/extra3/main.cpp" "/home/kei98/CLionProjects/Datos/Datos II/extra3/cmake-build-debug/CMakeFiles/extra3.dir/main.cpp.o"
+  "/home/kei98/CLionProjects/Datos/Datos II/extra3/node.cpp" "/home/kei98/CLionProjects/Datos/Datos II/extra3/cmake-build-debug/CMakeFiles/extra3.dir/node.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/extra3.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/extra3.dir/main.cpp.o"
+  "CMakeFiles/extra3.dir/node.cpp.o"
+  "CMakeFiles/extra3.dir/list.cpp.o"
+  "CMakeFiles/extra3.dir/collector.cpp.o"
+  "CMakeFiles/extra3.dir/CList.cpp.o"
+  "CMakeFiles/extra3.dir/CNode.cpp.o"
+  "extra3.pdb"
+  "extra3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/extra3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
